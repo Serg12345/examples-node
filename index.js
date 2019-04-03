20343647fca085b3f4fc41487e60dab9d7b47beb
Sergey <s2005s@mail.ru>
2019-04-03 23:28:25 +0300

    lesson 9 Работа с потоками

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// Для создания сервера подключаем модуль http
-var http = require('http');
+// методы createReadStream и createWriteStream позволяют передавать данные постепенно
+var fs = require('fs');
 
-// Функция createServer создаёт сервер
-var server = http.createServer(function(req, res) {
-	console.log("URL страницы: " + req.url); // Отслеживаем url запросы
-	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-	res.end('Привет');
+var myReadShort = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+
+myReadShort.on('data', function(chunk) {
+	console.log("новые данные получены:");
+	myWriteShort.write(chunk);
 });
-
-server.listen(3000, '127.0.0.1'); // Указываем порт и IP адрес
-console.log("Проверка работы порта 3000");
-
-// Для завершения работы сервера нажать ctrl + C
