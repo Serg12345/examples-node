ef5c5eaa17e5aa0141bfcbdb584782729cb29c1d
Sergey <s2005s@mail.ru>
2019-04-03 18:30:27 +0300

    lesson 7 Работа с директориями

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var fs = require('fs');
 
-// для асинхронного чтения файла используем метод .readFile(), пока файл не будет прочитан, дальнейший код не будет работать
-fs.readFile('text.txt', 'utf8', function(err, data) {
-	console.log(data);
+// для удаления файлов используются методы unlink(), unlinkSinc()
+// fs.unlink('some.txt', function() {});
+
+// метод mkdirSync() создаёт новую директорию (папку)
+// fs.mkdirSync('new-one');
+
+// метод rmdirSync() удаляет директорию (папку)
+// fs.rmdirSync('new-one');
+
+// Создадим папку и файл.
+// fs.mkdir('new-one', function() {
+// 	fs.writeFile('./new-one/some.txt', 'Создали файл.', function() {
+// 		console.log("Создали папку и файл.");
+// 	});
+// });
+
+// Удаляем файл и папку.
+fs.unlink('./new-one/some.txt', function() {
+	fs.rmdir('new-one', function() {});
 });
-
-
-// для асинхронной записи данных в существующий файл или в новый файл используем метод .writeFile()
-fs.writeFile('some.txt', 'Name', function(err, data) {});
-
-console.log("Test");
