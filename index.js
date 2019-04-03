3c6999bf16ab98db1dbdacd492f4cf8432db98ba
Sergey <s2005s@mail.ru>
2019-04-03 17:17:56 +0300

    lesson 6 Асинхронное чтение и запись файла

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 
-// для чтение файла используем метод .readFileSync(), пока файл не будет прочитан, дальнейший код не будет работать
-var file_readed = fs.readFileSync('text.txt', 'utf8');
-console.log(file_readed);
+// для асинхронного чтения файла используем метод .readFile(), пока файл не будет прочитан, дальнейший код не будет работать
+fs.readFile('text.txt', 'utf8', function(err, data) {
+	console.log(data);
+});
 
-// для записи данных в существующий файл или в новый файл используем метод .writeFileSync()
-var message = "Привет.\n " + file_readed;
-fs.writeFileSync('some_new_file.txt', message);
+
+// для асинхронной записи данных в существующий файл или в новый файл используем метод .writeFile()
+fs.writeFile('some.txt', 'Name', function(err, data) {});
+
+console.log("Test");
