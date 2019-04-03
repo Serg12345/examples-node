56dfe6731d3eff4d7dce0ff52cc2239064e8ce3e
Sergey <s2005s@mail.ru>
2019-04-03 13:18:39 +0300

    lesson 4 Множественный вывод из модуля Вариант 2

--- things.js
+++ things.js
@@ -1,16 +1,12 @@
 // Считаем количество элементов в массиве
+// Создание и экспорт именнованных функций
 
-var array_counter = function(array) {
+module.exports.array_counter = function(array) {
 	return "В массиве находится " + array.length + " элементов.";
 }
 
-var multiply = function(x, y) {
+module.exports.multiply = function(x, y) {
 	return `${x} умножить ${y} равно ${x * y}`; // запись в обратных одинарных кавычках
 }
 
-var some_value = 451;
-
-// Экспорт именованной функций
-module.exports.array_counter = array_counter;
-module.exports.multiply = multiply;
-module.exports.some_value = some_value;
+module.exports.some_value = 451;
