6ee4f0e8dece2a990fca8f43be251e1009acb9a9
Sergey <s2005s@mail.ru>
2019-04-04 10:54:51 +0300

    lesson 10 Функция pipe()

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // методы createReadStream и createWriteStream позволяют передавать данные постепенно
 var fs = require('fs');
 
-var myReadShort = fs.createReadStream(__dirname + '/article.txt', 'utf8');
-var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+// var myReadShort = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+// var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
 
-myReadShort.on('data', function(chunk) {
-	console.log("новые данные получены:");
-	myWriteShort.write(chunk);
+// функция pipe() записывает данные по частями (потоками)
+// myReadShort.pipe(myWriteShort);
+
+// Для создания сервера подключаем модуль http
+var http = require('http');
+
+// Функция createServer создаёт сервер
+var server = http.createServer(function(req, res) {
+	console.log("URL страницы: " + req.url); // Отслеживаем url запросы
+	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+	var myReadShort = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+	myReadShort.pipe(res); // используем pipe() для вывода прочитанных данных из файла article.txt 
 });
+
+server.listen(3000, '127.0.0.1'); // Указываем порт и IP адрес
+console.log("Проверка работы порта 3000");
